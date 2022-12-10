f9c39a37eb9bc393d63470d160d287ee29657b27
Odilon Damasceno <storckskey@gmail.com>
2022-12-10 10:58:07 -0300

    initial commit

---
+++ src/scene/scene.ts
@@ -0,0 +1,143 @@
+import {
+  Scene,
+  AxesHelper,
+  AmbientLight,
+  DirectionalLight,
+  Mesh,
+  SphereGeometry,
+  MeshToonMaterial,
+  PlaneGeometry,
+  Color,
+  SpotLight,
+  MeshMatcapMaterial,
+  MeshLambertMaterial,
+  MeshPhongMaterial,
+} from "three"
+import { renderer, updateRenderer } from "/src/core/renderer"
+
+import { gui } from "/src/core/gui"
+
+export const scene = new Scene()
+
+// Axes Helper
+const axesHelper = new AxesHelper(0.5)
+scene.add(axesHelper)
+
+gui.addInput(axesHelper, "visible", {
+  label: "AxesHelper",
+})
+
+const ambientLight = new AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const directionalLight = new DirectionalLight("#ffffff", 2)
+
+const directionalLightRed = new SpotLight("#ff0000", 2)
+
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 500
+directionalLight.shadow.normalBias = 0.5
+directionalLight.position.set(0.25, 2, 2.25)
+
+
+directionalLightRed.castShadow = true
+directionalLightRed.shadow.mapSize.set(1024, 1024)
+directionalLightRed.shadow.camera.far = 500
+directionalLightRed.shadow.normalBias = 0.5
+directionalLightRed.position.set(2.4, 5.9, -3.7)
+
+
+scene.add(directionalLight)
+scene.add(directionalLightRed)
+
+const PARAMS = {
+  color: "#5EDCAE",
+}
+
+const sphere = new Mesh(
+  new SphereGeometry(0.75, 32, 32),
+  new MeshPhongMaterial({
+    color: new Color(PARAMS.color),
+    wireframe: false,
+  })
+)
+
+sphere.position.set(0, 2, 0)
+sphere.castShadow = true
+
+const sphereCtrls = gui.addFolder({
+  title: "Sphere",
+})
+
+
+const lightCtrls = gui.addFolder({
+  title: "Light",
+})
+
+lightCtrls.addInput(directionalLightRed.position, "x", {
+  label: "pos x",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+lightCtrls.addInput(directionalLightRed.position, "y", {
+  label: "pos y",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+lightCtrls.addInput(directionalLightRed.position, "z", {
+  label: "pos z",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+
+
+
+sphereCtrls.addInput(sphere.position, "x", {
+  label: "pos x",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+sphereCtrls.addInput(sphere.position, "y", {
+  label: "pos y",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+sphereCtrls.addInput(sphere.position, "z", {
+  label: "pos z",
+  min: -10,
+  max: 10,
+  step: 0.1,
+})
+
+sphereCtrls.addInput(PARAMS, "color").on("change", (e) => {
+  sphere.material.color = new Color(e.value)
+})
+
+sphereCtrls.addInput(sphere.material, "wireframe")
+
+
+scene.add(sphere)
+
+const plane = new Mesh(
+  new PlaneGeometry(10, 10, 10, 10),
+  new MeshLambertMaterial({
+    color: new Color("#444"),
+    wireframe: false,
+  })
+)
+
+plane.receiveShadow = true
+plane.rotation.set(-Math.PI / 2, 0, 0)
+
+scene.add(plane)
+
+
+export function updateScene() {
+  updateRenderer()
+}
