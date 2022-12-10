9a190fe82f9bbcedb91f86544ff61128dafda605
Odilon Damasceno <storckskey@gmail.com>
2022-12-10 15:06:19 -0300

    fix: remove unecessery imports

--- src/scene/scene.ts
+++ src/scene/scene.ts
@@ -5,15 +5,13 @@ import {
   DirectionalLight,
   Mesh,
   SphereGeometry,
-  MeshToonMaterial,
   PlaneGeometry,
   Color,
   SpotLight,
-  MeshMatcapMaterial,
   MeshLambertMaterial,
   MeshPhongMaterial,
 } from "three"
-import { renderer, updateRenderer } from "../core/renderer"
+import { updateRenderer } from "../core/renderer"
 
 import { gui } from "../core/gui"
 
